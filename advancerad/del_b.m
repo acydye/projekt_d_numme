690264c47eb81ce571b5880dd526f7d9ea82e8bf
acydye <robinchatall@gmail.com>
2025-04-20 08:09:56 +0200

    Hello :wq

--- advancerad/del_b.m
+++ advancerad/del_b.m
@@ -1,11 +1,4 @@
-%Löser med ode-45 samt interpolation
-
-u0 = [0,0,-4.98,0]; %Start vektor
-h_t = 1e-3; %Initial steglängd för att beräkna tiden
-tol = 1e-10;
-tend = 1.22; %Avslut
-
-opts = odeset('AbsTol', 1e-5); %Inställningar för ode45
+; %Inställningar för ode45
 
 [t,y] = ode45(@dy_func,[0,tend],u0,opts);
 
@@ -30,34 +23,40 @@ function [t_meet,t_err_trunk] = find_time_adv(t,y,limit)
 
     r_r = y(:, 3:4); %Robot ortsvektor
     r_b = [zeros(length(y(:,1)),1),y(:,1)]; %Kulans orts vektor
-    s = vecnorm(r_r-r_b,2,2);
-
-    %Central differans metod
-    ds_diff = diff(s)./diff(t); %första
-
-    ds_2_diff = diff(ds_diff)./diff(t(1:end-1)); %andra
-
-    %Gräns för ändring i derivatan
-    tol_max = limit*max(ds_2_diff);
-    i = find(ds_2_diff > tol_max,1, 'first'); %Omvandlar till rätt index
-
-
-    subplot(2,2,1)
-    plot(t(1:length(ds_diff)),ds_diff)
-    subplot(2,2,2)
-    plot(t(1:length(ds_2_diff)),ds_2_diff)
+    robot_ball = r_b-r_r;
+    s = vecnorm(robot_ball,2,2);
+    
    
-   
-    subplot(2,2,3)
-    plot(t(i-5:i+2),s(i-5:i+2), 'blue', t(i),s(i),'x')
-    subplot(2,2,4)
-    plot(t,s, 'red',t(i),s(i),'X')
+    %hitta index
+    x = robot_ball(:,1); y = robot_ball(:,2);
+    i_x = find(x < 0, 1);
+    i_y = find(abs(y) <= abs(y(i_x)));
+    i = i_x
+    
+
+
+
+    plot(robot_ball(:,1),robot_ball(:,2))
+    
+
+
+    % 
+    % subplot(2,2,1)
+    % plot(t(1:length(ds_diff)),ds_diff)
+    % subplot(2,2,2)
+    % plot(t(1:length(ds_2_diff)),ds_2_diff)
+    % 
+    % 
+    % subplot(2,2,3)
+    % plot(t(i-5:i+2),s(i-5:i+2), 'blue', t(i),s(i),'x')
+    % subplot(2,2,4)
+    % plot(t,s, 'red',t(i),s(i),'X')
 
 
 
     % Vill nu interpolera för punkterna innan och hitta t där d(t) = 0
 
-    span = i-2:i; %Index av punkter som ska interpoleras
+    span = i-2:i %Index av punkter som ska interpoleras
     dd = s(span); tt = t(span); %avståndet (dd) och tiden (tt)
     t_ca = t(i); %Ungefärliga värdet
 
@@ -66,7 +65,7 @@ function [t_meet,t_err_trunk] = find_time_adv(t,y,limit)
 
     [~, index_root] = min(abs(roots_sec-t_ca));
     root_sec = roots_sec(index_root);
-    
+
 
     third_koef = polyfit(tt,dd,3); %Andra grads interpolation
     roots_trd = roots(third_koef);
